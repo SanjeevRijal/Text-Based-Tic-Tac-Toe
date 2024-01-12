b1858605d1788a4c8657dc323e4eafa660cc0c38
Sanjeev Rijal <hopsanjeev@gmail.com>
2024-01-13 08:41:21 +1100

    turn count included and feature improvement

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 
-player_score = 0
-computer_score= 0
-restart_game = 1
-
 def print_board():
     for rows in board:
         print (rows[0],rows[1],rows[2],rows[3],rows[4])
 
 
-def update_board(player_sign, user_input):
+def update_board(player_sign, player_input):
     # updating board index after each user inputs
     for row in board:
-        if user_input in row:
-            index_to_update = row.index(user_input)
+        if player_input in row:
+            index_to_update = row.index(player_input)
             row[index_to_update] = player_sign
     print_board()
 
@@ -44,21 +40,35 @@ def winner_checker(player_sign):
         return True
 
 def winner_announcer():
-    if computer_score > player_score:
-        print(f"Computer win.\nComputer Score: {computer_score}\nPlayer Score: {player_score}")
-    elif player_score > computer_score:
-        print(f"Player win.\nPlayer Score: {player_score}\nComputer Score: {computer_score}")
+    if player_X > player_O:
+        print(f"Computer win.\nPlayer X: {player_X}\nPlayer O: {player_O}")
+    elif player_O > player_X:
+        print(f"Player win.\nPlayer O: {player_O}\nPlayer X: {player_X}")
     else:
-        print(f"It's a tie.\nComputer Score: {computer_score}\nPlayer Score: {player_score}")
+        print(f"It's a tie.\nPlayer X: {player_X}\nPlayer O: {player_O}")
+
+def already_enter_number_checker():
+    user_input = input("Type in a number:  ")
+    if user_input in already_selected_number:
+        print(f"{user_input} is already used, choose another number")
+        already_enter_number_checker()
+    else:
+        return user_input
+
+def turn_counter(turn):
+    if turn > 8:
+        print("It's a tie.")
+        print(f"Current Score\nPlayer X: {player_X}\nPlayer O:{player_O} ")
+        return True
 
 
-
-
-
-already_entered_list = []
-computer_sing = "X"
-user_sign = "O"
-while restart_game == 1:
+already_selected_number = []
+player_O = 0
+player_X = 0
+turn = 0
+player_X_sign = "X"
+player_O_sign = "O"
+while True:
     board = [
         ['1', '|', '2', '|', '3'],
         ['-', '-', '-', '-', '-'],
@@ -68,48 +78,44 @@ while restart_game == 1:
     ]
     print_board()
     while True:
-        if not winner_checker(computer_sing):
+        if not winner_checker(player_X_sign):
             print("\n")
-            print("*"*10 + "Computer Turn" + "*"*10)
-            user_input = input("Type in a number:  ")
-            update_board(computer_sing, user_input)
+            print("*"*10 + "Player X" + "*"*10)
+            user_input = already_enter_number_checker()
+            update_board(player_X_sign, user_input)
+            already_selected_number.append(user_input)
+            turn +=1
 
-        if winner_checker(computer_sing):
-            computer_score+=1
-            print(f"Current Score\nComputer Score: {computer_score}\nUser Score:{player_score} ")
-            play_again = input("Do you want to play again(Y/N : ")
-            if play_again == "Y":
-                restart_game = 1
-                break
-            if play_again =="N":
-                winner_announcer()
-                restart_game = 6
-                break
+        if winner_checker(player_X_sign):
+            player_X += 1
+            print(f"Current Score\nPlayer X: {player_X}\nPlayer Y:{player_O} ")
+            break
 
-        if restart_game==1:
+        if turn_counter(turn):
+            break
 
-            if not winner_checker(user_sign):
-                print("*" * 10 + "Player Turn" + "*" * 10)
-                user_input = input("Type in a number:  ")
-                update_board(user_sign, user_input)
-
-            if winner_checker(user_sign):
-                player_score +=1
-                print(f"Current Score\nComputer Score: {computer_score}\nUser Score:{player_score} ")
-                play_again= input("Do you want to play again(Y/N) : ")
-                if play_again == "Y":
-                    restart_game =1
-                    break
-                else:
-                    restart_game = 2
-                    winner_announcer()
-                    break
+        if not winner_checker(player_O_sign):
+            print("*" * 10 + "Player O" + "*" * 10)
+            user_input = already_enter_number_checker()
+            update_board(player_O_sign, user_input)
+            already_selected_number.append(user_input)
+            turn +=1
 
 
+        if winner_checker(player_O_sign):
+            player_O +=1
+            print(f"Current Score\nPlayer X: {player_X}\nPlayer O:{player_O} ")
+            break
 
+        if turn_counter(turn):
+            break
 
-
-
-
+    play_again = input("Do you want to play again(Y/N):  ")
+    if play_again.upper()=="N":
+        winner_announcer()
+        break
+    else:
+        already_selected_number=[]
+        turn=0
 
 
